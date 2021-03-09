9f6345b82bccb8b0320a481fb8934c6224a472f4
Denner Ramiro Ribeiro <dennerramiror@gmail.com>
2021-03-09 11:59:35 -0300

    Swagger Done

--- Hotelaria/CrudService/src/main/java/com/rp4/hotelaria/controller/HotelController.java
+++ Hotelaria/CrudService/src/main/java/com/rp4/hotelaria/controller/HotelController.java
@@ -1,24 +1,18 @@
 package com.rp4.hotelaria.controller;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.rp4.hotelaria.dto.HotelDTO;
 import com.rp4.hotelaria.interfaces.IHotelService;
 import com.rp4.hotelaria.model.Hotel;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/hoteis")
+
 public class HotelController {
     private IHotelService hotelService;
 
@@ -27,6 +21,7 @@ public class HotelController {
     }
 
     @PostMapping("/salvar")
+    @ApiOperation(value = "Salvar hotel")
     public void saveHotel(@RequestBody HotelDTO hotelDTO){
         Hotel hotel = new Hotel();
         hotel.setCidade(hotelDTO.getCidade());
@@ -39,22 +34,26 @@ public class HotelController {
     }
 
     @GetMapping("/hotel/{id}")
+    @ApiOperation(value = "Retornar hotel")
     public ResponseEntity<?> pegarHotelPeloId(@PathVariable("id")Long id){
     	Hotel hotel = hotelService.getHotelById(id);
     	return new ResponseEntity<> (hotel, HttpStatus.OK);
     }
     
     @PutMapping("/hotel/atualizar")
+    @ApiOperation(value = "Atualizar Hotel")
     public Hotel atualizarHotel(@RequestBody Hotel hotel) {
     	return hotelService.atualizarHotel(hotel);
     }
     
     @DeleteMapping("/hotel/excluir/{id}")
+    @ApiOperation(value = "Deletar hotel")
     public void deleteHotel(@PathVariable("id")Long id) {
     	hotelService.excluirHotel(id);
     }
 
     @GetMapping("/todos")
+    @ApiOperation(value = "Retornar uma lista de hotéis")
     public List<Hotel> pegarTodosHoteis(){
     	List<Hotel> hoteis = hotelService.pegarTodosHoteis();
     	return hoteis;
