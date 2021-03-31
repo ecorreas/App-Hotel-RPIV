2d01662cfb8646cf429048db22948a0962b86464
ecorreas <56318574+ecorreas@users.noreply.github.com>
2021-03-30 23:52:28 -0300

    Movendo a classe MyIcons.dart

--- FrontEnd/lib/Assets/Icons/MyIcons.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const iconHome = 'lib/Assets/Icons/inicio.svg';
-const logo = 'lib/Assets/Icons/logo.png';
-const tela_login = 'lib/Assets/Icons/telalogin.svg';
