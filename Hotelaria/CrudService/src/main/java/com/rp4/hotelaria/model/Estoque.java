19db400574d66881ff3f90802cb22aa1bbc6fe43
Denner Ramiro Ribeiro <dennerramiror@gmail.com>
2021-04-13 18:17:53 -0300

    Adding Estoque

---
+++ Hotelaria/CrudService/src/main/java/com/rp4/hotelaria/model/Estoque.java
@@ -0,0 +1,65 @@
+package com.rp4.hotelaria.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tbl_controle_estoque")
+public class Estoque {
+
+    @Id
+    @Column(name = "id_estoque")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "quantidade")
+    private int quantidade;
+
+    @Column(name = "valor")
+    private double valor;
+
+    public Estoque(){
+
+    }
+
+    public Estoque(String nome, int quantidade, double valor){
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
